3d6d86d687903bd4f98d2e40df5c00c0b4f06954
codecanna <codecannamw@gmail.com>
2025-12-11 13:55:58 -0700

    Still working on this.

--- handlers/view_entries.ts
+++ handlers/view_entries.ts
@@ -1,7 +1,16 @@
 import { Context } from "grammy";
 import { Conversation } from "@grammyjs/conversations";
 import { getEntriesByUserId } from "../models/entry.ts";
+import { Entry } from "../types/types.ts";
 
 export async function view_entries(conversation: Conversation, ctx: Context) {
-    const entries = getEntriesByUserId(ctx.from?.id!);
+    // const entriesQueryResults = getEntriesByUserId(ctx.from?.id!);
+
+    // const entries: Entry[] = [];
+    // for (let i = 0; i < entriesQueryResults.length; i++) {
+    //     const entry: Entry = {
+    //         id: entriesQueryResults[i].id!,
+
+    //     }
+    // }
 }
