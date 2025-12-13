1cfc8ae07dfddb3946ef695c822c1e3f3bb698d1
codecanna <codecannamw@gmail.com>
2025-12-12 22:33:15 -0700

    added helpString

--- constants/strings.ts
+++ constants/strings.ts
@@ -1,5 +1,4 @@
 export const startString: string = `Hello!  Welcome to JotBot.  I'm here to help you record your emotions and emotion!`;
-export const helpString: string = `This is a help message, I hope its helpful!!`;
 export const telegramDownloadUrl = "https://api.telegram.org/file/bot<token>/<file_path>";
 
 export const crisisString = 
@@ -26,6 +25,30 @@ Councelors specializing in LGBTQ+
 
 <b><u>Life Threatening Emergencies</u></b>
 Call 911
+`
+
+export const helpString: string = 
+`
+<b>Jotbot help</b>
+<b><u>What is Jotbot?</u></b>
+Jotbot is a telegram bot that can help you to record your thoughts and emotions in directly in the telegram app.
+
+<b><u>How do I use Jotbot?</u></b>
+Jotbot is easy to use you have to be "registered" to start recording entries.
+Once this is done you can use /new_entry to start recording an entry.  You just answer the bot's questions to the best of you ability from there.
+
+After you are finished recording your entry you can view your entry by using /view_entries.  This will bring up a menu that let's you scroll through your entries.  You can also delete entries from this screen.
+If you are wanting to stop using Jotbot you can delete your account using /delete_account this will also delete all of your journal entries!
+
+<b><u>Commands</u></b>
+/start - Start the bot, if it's your first time messaging the bot you will be asked if you want to register.
+/help - Prints this help string in a message
+/new_entry - Start a new entry
+/view_entries - Scroll through your entries
+/delete_account - Delete your accound plus all entries
+/🆘 - Show the crisis help lines
+
+<b>NOTE</b>: The selfie features aren't working right now.
 `
 
 export enum Emotions {
