f5dbb109d5f706fd085cd11f4a8e8ff6ae9e2fdc
codecanna <codecannamw@gmail.com>
2025-12-21 07:50:56 -0700

    added getSettingsById()

--- models/settings.ts
+++ models/settings.ts
@@ -46,3 +46,32 @@ export function updateSettings(
     console.error(`Failed to update user ${userId} settings: ${err}`);
   }
 }
+
+export function getSettingsById(userId: number, dbFile: PathLike) {
+  try {
+    const db = new DatabaseSync(dbFile);
+
+    if (
+      !(db.prepare("PRAGMA integrity_check;").get()?.integrity_check === "ok")
+    ) throw new Error("JotBot Error: Databaes integrety check failed!");
+    db.exec("PRAGMA foreign_keys = ON;");
+
+    const queryResult = db.prepare(
+      `SELECT * FROM settings_db WHERE userId = ${userId}`,
+    ).get();
+
+    db.close();
+    return {
+      id: Number(queryResult?.id!),
+      userId: Number(queryResult?.userId!),
+      storeMentalHealthInfo: Boolean(
+        Number(queryResult?.storeMentalHealthInfo!),
+      ),
+      selfieDirectory: String(queryResult?.selfieDirectory!),
+    };
+  } catch (err) {
+    console.error(
+      `Failed to retrieve user ${userId} settings from ${dbFile}: ${err}`,
+    );
+  }
+}
