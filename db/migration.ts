fb0995e824a9e69cc7fd0dbc935d008daca166fb
codecanna <codecannamw@gmail.com>
2025-12-22 14:27:20 -0700

    Got rid of hard coded queries

--- db/migration.ts
+++ db/migration.ts
@@ -1,133 +1,110 @@
 import { PathLike } from "node:fs";
 import { DatabaseSync } from "node:sqlite";
+import { sqlFilePath } from "../constants/paths.ts";
 
-export function createEntryTable(dbFile: PathLike) {
+export async function createEntryTable(dbFile: PathLike) {
   try {
     const db = new DatabaseSync(dbFile);
+    const query =
+      (await Deno.readTextFile(`${sqlFilePath}/create_entry_table.sql`)).trim();
     db.exec("PRAGMA foreign_keys = ON;");
-    db.prepare(`
-      CREATE TABLE IF NOT EXISTS entry_db (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        userId INTEGER NOT NULL,
-        timestamp INTEGER NOT NULL,
-        lastEditedTimestamp INTEGER,
-        situation TEXT NOT NULL, 
-        automaticThoughts TEXT NOT NULL,
-        emotionName TEXT NOT NULL,
-        emotionEmoji TEXT,
-        emotionDescription TEXT NOT NULL,
-        selfiePath TEXT,
-        FOREIGN KEY (userId) REFERENCES user_db(telegramId)
-        ON DELETE CASCADE
-      );
-    `).run();
+    db.prepare(query).run();
     db.close();
   } catch (err) {
     console.error(`Failed to create entry_db table: ${err}`);
   }
 }
 
-export function createGadScoreTable(dbFile: PathLike) {
+export async function createGadScoreTable(dbFile: PathLike) {
   try {
     const db = new DatabaseSync(dbFile);
+    const query =
+      (await Deno.readTextFile(`${sqlFilePath}/create_gad_score_table.sql`))
+        .trim();
     db.exec("PRAGMA foreign_keys = ON;");
-    db.prepare(`
-      CREATE TABLE IF NOT EXISTS gad_score_db (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        userId INTEGER UNIQUE,
-        timestamp INTEGER NOT NULL,
-        score INTEGER NOT NULL,
-        severity TEXT NOT NULL,
-        impact TEXT NOT NULL,
-        FOREIGN KEY (userId) REFERENCES user_db(telegramId)
-        ON DELETE CASCADE
-      );
-    `).run();
+    db.prepare(query).run();
     db.close();
   } catch (err) {
     console.error(`There was a a problem create the user_db table: ${err}`);
   }
 }
 
-export function createPhqScoreTable(dbFile: PathLike) {
+export async function createPhqScoreTable(dbFile: PathLike) {
   try {
     const db = new DatabaseSync(dbFile);
+    const query =
+      (await Deno.readTextFile(`${sqlFilePath}/create_phq_score_table.sql`))
+        .trim();
     db.exec("PRAGMA foreign_keys = ON;");
-    db.prepare(`
-      CREATE TABLE IF NOT EXISTS phq_score_db (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        userId INTEGER UNIQUE,
-        timestamp INTEGER NOT NULL,
-        score INTEGER NOT NULL,
-        severity TEXT NOT NULL,
-        action TEXT NOT NULL,
-        impact TEXT NOT NULL,
-        FOREIGN KEY (userId) REFERENCES user_db(telegramId)
-        ON DELETE CASCADE
-      );
-    `).run();
+    db.prepare(query).run();
     db.close();
   } catch (err) {
     console.error(`There was a a problem create the user_db table: ${err}`);
   }
 }
 
-export function createUserTable(dbFile: PathLike) {
+export async function createUserTable(dbFile: PathLike) {
   try {
     const db = new DatabaseSync(dbFile);
+    const query = (await Deno.readTextFile(
+      `${sqlFilePath}/create_user_table.sql`,
+    )).trim();
     db.exec("PRAGMA foreign_keys = ON;");
-    db.prepare(`
-      CREATE TABLE IF NOT EXISTS user_db (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        telegramId INTEGER UNIQUE,
-        username TEXT NOT NULL UNIQUE,
-        dob INTEGER NOT NULL,
-        joinedDate INTEGER NOT NULL
-      );
-    `).run();
+    db.prepare(query).run();
     db.close();
   } catch (err) {
     console.error(`There was a a problem create the user_db table: ${err}`);
   }
 }
 
-export function createSettingsTable(dbFile: PathLike) {
+export async function createSettingsTable(dbFile: PathLike) {
   try {
     const db = new DatabaseSync(dbFile);
+    const query =
+      (await Deno.readTextFile(`${sqlFilePath}/create_settings_table.sql`))
+        .trim();
     db.exec("PRAGMA foreign_keys = ON;");
-    db.prepare(`
-      CREATE TABLE IF NOT EXISTS settings_db (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        userId INTEGER,
-        storeMentalHealthInfo INTEGER DEFAULT 0,
-        FOREIGN KEY (userId) REFERENCES user_db(telegramId)
-        ON DELETE CASCADE
-      );
-    `).run();
+    db.prepare(query).run();
     db.close();
   } catch (err) {
     console.error(`Failed to create settings table: ${err}`);
   }
 }
 
-export function createJournalTable(dbFile: PathLike) {
+export async function createJournalTable(dbFile: PathLike) {
   try {
     const db = new DatabaseSync(dbFile);
+    const query =
+      (await Deno.readTextFile(`${sqlFilePath}/create_journal_table.sql`))
+        .trim();
     db.exec("PRAGMA foreign_keys = ON;");
-    db.prepare(`
-      CREATE TABLE IF NOT EXISTS journal_db (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        userId INTEGER NOT NULL,
-        timestamp INTEGER NOT NULL,
-        lastEditedTimestamp INTEGER,
-        content TEXT NOT NULL,
-        length INTEGER NOT NULL,
-        images TEXT,
-        voiceRecordings TEXT,
-        FOREIGN KEY (userId) REFERENCES user_db(telegramId)
-        ON DELETE CASCADE
-      );
-    `).run();
+    db.prepare(query).run();
+    db.close();
+  } catch (err) {
+    console.error(`Failed to create settings table: ${err}`);
+  }
+}
+
+export async function createJournalEntryPhotosTable(dbFile: PathLike) {
+  try {
+    const db = new DatabaseSync(dbFile);
+    const query =
+      (await Deno.readTextFile(`${sqlFilePath}/create_photo_table.sql`)).trim();
+    db.exec("PRAGMA foreign_keys = ON;");
+    db.prepare(query).run();
+    db.close();
+  } catch (err) {
+    console.error(`Failed to create settings table: ${err}`);
+  }
+}
+
+export async function createVoiceRecordingTable(dbFile: PathLike) {
+  try {
+    const db = new DatabaseSync(dbFile);
+    const query =
+      (await Deno.readTextFile(`${sqlFilePath}/create_voice_recording_table.sql`)).trim();
+    db.exec("PRAGMA foreign_keys = ON;");
+    db.prepare(query).run();
     db.close();
   } catch (err) {
     console.error(`Failed to create settings table: ${err}`);
