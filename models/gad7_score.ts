82e6d8c8bafc67da6a1988eb65c1e2e9928a5e47
codecanna <codecannamw@gmail.com>
2025-12-23 05:53:42 -0700

    added get score byt id function

--- models/gad7_score.ts
+++ models/gad7_score.ts
@@ -1,6 +1,10 @@
 import { DatabaseSync } from "node:sqlite";
 import { GAD7Score } from "../types/types.ts";
 import { PathLike } from "node:fs";
+import { sqlFilePath } from "../constants/paths.ts";
+import { anxietySeverityStringToEnum } from "../utils/misc.ts";
+
+const sqlPath = `${sqlFilePath}/gad_score`;
 
 /**
  * Insert GAD-7 score into gad_score_db table
@@ -20,12 +24,13 @@ export function insertGadScore(score: GAD7Score, dbPath: PathLike) {
     }
 
     queryResult = db.prepare(
-      `INSERT INTO gad_score_db (userId, timestamp, score, severity, impact) VALUES (?, ?, ?, ?, ?);`,
+      `INSERT INTO gad_score_db (userId, timestamp, score, severity, action, impactQuestionAnswer) VALUES (?, ?, ?, ?, ?, ?);`,
     ).run(
       score.userId,
       score.timestamp,
       score.score,
       score.severity,
+      score.action,
       score.impactQuestionAnswer,
     );
 
@@ -36,6 +41,7 @@ export function insertGadScore(score: GAD7Score, dbPath: PathLike) {
     db.close();
   } catch (err) {
     console.error(`Failed to insert gad-7 score: ${err}`);
+    throw new Error(`Failed to insert GAD-7 score: ${err}`);
   }
   console.log(queryResult);
   return queryResult;
@@ -49,9 +55,43 @@ export function insertGadScore(score: GAD7Score, dbPath: PathLike) {
 //   // TODO
 // }
 
-// export function getGadScoreById(id: number) {
-//   // TODO
-// }
+/**
+ * 
+ * @param id 
+ * @param dbPath 
+ * @returns 
+ */
+export function getGadScoreById(id: number, dbPath: PathLike): GAD7Score {
+  let gadScore;
+  try {
+    const db = new DatabaseSync(dbPath);
+    const query = Deno.readTextFileSync(`${sqlPath}/get_gad_score_by_id.sql`)
+      .replace("<ID>", id.toString()).trim();
+    db.exec("PRAGMA foreign_keys = ON;");
+    if (
+      !(db.prepare("PRAGMA integrity_check;").get()?.integrity_check === "ok")
+    ) {
+      throw new Error("JotBot Error: Databaes integrety check failed!");
+    }
+
+    gadScore = db.prepare(query).get();
+    console.log(gadScore);
+
+    db.close();
+  } catch (err) {
+    console.error(`Failed to insert gad-7 score: ${err}`);
+    throw new Error(`Failed to insert GAD-7 score: ${err}`);
+  }
+  return {
+    id: Number(gadScore?.id),
+    userId: Number(gadScore?.userId),
+    timestamp: Number(gadScore?.timestamp),
+    score: Number(gadScore?.score),
+    severity: anxietySeverityStringToEnum(gadScore?.severity?.toString()!),
+    action: gadScore?.action?.toString()!,
+    impactQuestionAnswer: gadScore?.impactQuestionAnswer?.toString()!,
+  };
+}
 
 // export function getAllGadScoresByUserId(userId: number) {
 //   // TODO
